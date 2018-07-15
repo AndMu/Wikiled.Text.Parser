e1063d68304af3bd5c8f92c54bc636d61bb06d52
Andrius Mudinas <keistokas@gmail.com>
2018-07-15 02:03:07 +0100

    switch to core

---
+++ src/Wikiled.Text.Parser.Tests/Global.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Wikiled.Text.Parser.Tests
+{
+    [SetUpFixture]
+    public class Global
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+
+        [OneTimeTearDown]
+        public void Clean()
+        {
+        }
+    }
+}
